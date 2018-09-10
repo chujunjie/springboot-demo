0bc0359fc88e01837f883426b1ac19628ea8f5c9
chujunjie <c.junjie@hotmail.com>
2018-09-10 17:25:49 +0800

    nettySimpleDemo

---
+++ src/main/java/com/example/srpingbootjdbc/netty/nettySimpleDemo/EchoServerHandler.java
@@ -0,0 +1,55 @@
+package com.example.srpingbootjdbc.netty.nettySimpleDemo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @Description: 定义处理入站事件的方法
+ * @Author: chujunjie
+ * @Date: Create in 11:13 2018/9/10
+ * @Modified By
+ */
+@ChannelHandler.Sharable //标识这类的实例之间可以在 channel 里面共享
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 信息入站时调用
+     * @param ctx
+     * @param msg
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx,
+                            Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8)); //日志消息输出到控制台
+        ctx.write(in); //将所接收的消息返回给发送者
+    }
+
+    /**
+     * 通知处理器最后的 channelread() 是当前批处理中的最后一条消息时调用
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)//冲刷所有待审消息到远程节点。关闭通道后，操作完成
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 读操作时捕获到异常时调用
+     * @param ctx
+     * @param cause
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        cause.printStackTrace(); //打印异常堆栈跟踪
+        ctx.close(); //关闭通道
+    }
+}
