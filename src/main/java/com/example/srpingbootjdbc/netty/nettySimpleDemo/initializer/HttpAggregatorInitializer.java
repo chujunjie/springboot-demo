66b2b0742c7cfb4f81b4590b1335abdf307672e7
chujunjie <c.junjie@hotmail.com>
2018-09-11 14:53:24 +0800

    Http、Https、WebSocket

---
+++ src/main/java/com/example/srpingbootjdbc/netty/nettySimpleDemo/initializer/HttpAggregatorInitializer.java
@@ -0,0 +1,38 @@
+package com.example.srpingbootjdbc.netty.nettySimpleDemo.initializer;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.*;
+
+/**
+ * @Description: HTTP消息聚合
+ *               这个操作有一个轻微的成本,消息段需要缓冲,直到完全可以将消息转发到下一个 ChannelInboundHandler 管道。
+ *               但好处是,不必担心消息碎片,总是能够看到完整的消息内容。
+ *
+ *               使用 HTTP 时建议压缩数据以减少传输流量，但是压缩数据会增加 CPU 负载。
+ * @Author: chujunjie
+ * @Date: Create in 13:41 2018/9/11
+ * @Modified By
+ */
+public class HttpAggregatorInitializer extends ChannelInitializer<Channel> {
+
+    private final boolean client;
+
+    public HttpAggregatorInitializer(boolean client) {
+        this.client = client;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (client) {
+            pipeline.addLast("codec", new HttpClientCodec());  //client: 添加 HttpClientCodec
+            pipeline.addLast("decompressor",new HttpContentDecompressor());  //添加 HttpContentDecompressor 用于处理来自服务器的压缩的内容
+        } else {
+            pipeline.addLast("codec", new HttpServerCodec());  //server: server 模式时添加 HttpServerCodec
+            pipeline.addLast("compressor",new HttpContentCompressor()); //HttpContentCompressor 用于压缩来自 client 支持的 HttpContentCompressor
+        }
+        pipeline.addLast("aggregator", new HttpObjectAggregator(512 * 1024));  //添加 HttpObjectAggregator 到 ChannelPipeline, 使用最大消息值是 512kb
+    }
+}
---
+++ src/main/java/com/example/srpingbootjdbc/netty/nettySimpleDemo/initializer/SslChannelInitializer.java
@@ -0,0 +1,46 @@
+package com.example.srpingbootjdbc.netty.nettySimpleDemo.initializer;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * @Description: 启用HTTPS协议，运用SSL/TLS加密
+ *               在大多数情况下,SslHandler 将成为 ChannelPipeline 中的第一个 ChannelHandler 。
+ *               这将确保所有其他 ChannelHandler 应用他们的逻辑到数据后加密后才发生,从而确保他们的变化是安全的。
+ * @Author: chujunjie
+ * @Date: Create in 11:17 2018/9/11
+ * @Modified By
+ */
+public class SslChannelInitializer extends ChannelInitializer<Channel> {
+
+    private final SslContext context;
+    private final boolean startTls;
+    private final boolean client;
+
+    public SslChannelInitializer(SslContext context, boolean startTls, boolean client) { //使用构造函数来传递 SSLContext 用于使用(startTls 是否启用)
+        this.context = context;
+        this.startTls = startTls;
+        this.client = client;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        SSLEngine engine = context.newEngine(ch.alloc());  //从 SslContext 获得一个新的 SslEngine 。给每个 SslHandler 实例使用一个新的 SslEngine
+        engine.setUseClientMode(true); //设置 SslEngine 是 client 或者是 server 模式
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addFirst("ssl", new SslHandler(engine, startTls));  //添加 SslHandler 到 pipeline 作为第一个处理器以启用 HTTPS
+
+        if (client) {
+            pipeline.addLast("codec", new HttpClientCodec()); //添加 HttpClientCodec
+        } else {
+            pipeline.addLast("codec", new HttpServerCodec());  //Server模式：添加 HttpServerCodec
+        }
+    }
+}
