cf0f261d3f28f8ae0f012d463b601684475a1084
chujunjie <c.junjie@hotmail.com>
2018-09-27 23:10:37 +0800

    StreamAPI

---
+++ src/main/java/com/example/srpingbootjdbc/java8Test/StreamTest.java
@@ -0,0 +1,24 @@
+package com.example.srpingbootjdbc.java8Test;
+
+import java.util.List;
+
+import static com.example.srpingbootjdbc.java8Test.Dish.menu;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @Description:
+ * @Author: chujunjie
+ * @Date: Create in 15:50 2018/9/21
+ * @Modified By
+ */
+public class StreamTest {
+    public static void main(String[] args) {
+        List<String> threeHighCaloricDishNames = menu.stream()
+                .filter(d -> d.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(toList());
+        threeHighCaloricDishNames.forEach(System.out::println);
+    }
+}
+
