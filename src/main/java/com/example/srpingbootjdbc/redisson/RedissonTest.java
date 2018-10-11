34625f454fc5dcad53bade45117563ac61401334
chujunjie <c.junjie@hotmail.com>
2018-10-12 00:03:31 +0800

    redisson实现分布式锁

--- src/main/java/com/example/srpingbootjdbc/redisson/RedissonTest.java
+++ src/main/java/com/example/srpingbootjdbc/redisson/RedissonTest.java
@@ -2,6 +2,7 @@ package com.example.srpingbootjdbc.redisson;
 
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
+import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
@@ -16,24 +17,44 @@ import java.util.concurrent.TimeUnit;
  * @Modified By
  */
 public class RedissonTest {
+    private static String prefix = "redis://";
+    private static String LOCK_01 = "lock_01";
+    private static String LOCK_02 = "lock_02";
+    private static String MAP_01 = "map_01";
+    private static String MAP_02 = "map_02";
+
     public static void main(String[] args) {
-        String recordId = "record_id";
-        String prefix = "redis://";
 
         Config config = new Config();
         config.useSingleServer().setAddress(prefix + "localhost:6379");
         RedissonClient client = Redisson.create(config);
-        RLock rLock = client.getLock(recordId);
+
+//        client.getMap(MAP_01).put(1, "hello");
+//        client.getMap(Map_02).put(1, "world");
+
+        /* 获取不同的锁，可以实现并行操作 */
+        new Thread(() -> tryLock(LOCK_01, MAP_01, client), "线程1").start();
+
+        new Thread(() -> tryLock(LOCK_02, MAP_02, client), "线程2").start();
+    }
+
+    private static void tryLock(String lockName, String recordId, RedissonClient client) {
+
+        RLock rLock = client.getLock(lockName);
+
         try {
-            boolean b = rLock.tryLock(5, 6, TimeUnit.SECONDS);
+            boolean b = rLock.tryLock(5, 10, TimeUnit.SECONDS);
             if (b) {
-                System.out.println("recordId: " + recordId + "获取到锁");
+                RMap<Integer, String> map = client.getMap(recordId);
+                String value = map.get(1);
+                System.out.println(Thread.currentThread().getName() + "获取到锁，值为：" + value);
+            } else {
+                System.out.println("当前锁被其他线程占用");
             }
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        } finally {
-            rLock.unlock();
         }
-        client.shutdown();
+
+
     }
 }
