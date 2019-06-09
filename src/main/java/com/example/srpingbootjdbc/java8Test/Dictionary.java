9422900f3950a3b29a5698009ec907a723e4794c
chujunjie <Aaron1995>
2019-06-09 12:58:17 +0800

    comparator

--- src/main/java/com/example/srpingbootjdbc/java8Test/Dictionary.java
+++ src/main/java/com/example/srpingbootjdbc/java8Test/Dictionary.java
@@ -1,14 +1,31 @@
 package com.example.srpingbootjdbc.java8Test;
 
+import java.util.Comparator;
+
+import static java.util.Comparator.comparingInt;
+
 /**
  * @Description:
  * @Author: chujunjie
  * @Date: Create in 23:25 2019/5/30
  * @Modified By
  */
-public class Dictionary {
+public class Dictionary implements Comparable<Dictionary> {
 
-    public boolean check(String word){
+    private int order;
+
+    private int next;
+
+    private static final Comparator<Dictionary> COMPARATOR =
+            comparingInt((Dictionary dic) -> dic.order)
+                    .thenComparingInt(dic -> dic.next);
+
+    public boolean check(String word) {
         return true;
+    }
+
+    @Override
+    public int compareTo(Dictionary o) {
+        return COMPARATOR.compare(this, o);
     }
 }
